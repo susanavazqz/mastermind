f57c3589b2ba4c5a09f7429390cd4d6d81039a8d
Susana Vázquez <svazquez@netquest.com>
2019-01-28 07:26:12 +0100

    [FIX] fixed test

--- tests/test_game.py
+++ tests/test_game.py
@@ -13,9 +13,10 @@ class TestGame(TestCase):
     def test_create_open_game(self):
         self.assertEqual(self.game.close, False)
 
-    def test_finish_game(self,):
-        self.game.finish_game()
-        self.assertEqual(self.game.close, True)
+    def test_finish_game(self):
+        self.game.codebreaker.guess_code = 'B,P,W,R'
+        self.game.codemaker.code = 'B,P,W,R'
+        self.assertEqual(self.game._is_finished_game(), True)
 
     def test_get_black_pegs(self):
         self.game.codebreaker.guess_code = 'B,P,W,R'
