e66a8fc710fa4509c72242941356ca15da3d9fb5
Susana Vázquez <svazquez@netquest.com>
2019-01-28 07:05:35 +0100

    [ADD] Added sqlalchemy configuration

--- app.py
+++ app.py
@@ -2,9 +2,21 @@ from flask import Flask
 from flask_restful import Api
 from resources.game import GameResource
 
+from db import db
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
 api = Api(app)
+
+db.init_app(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 
 api.add_resource(GameResource, '/game', '/game/<int:game_id>')
